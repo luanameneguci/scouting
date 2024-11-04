49a2bde817f9b7bbda90b44554b99a80682bb42e
pt1C3 <rafaelsilva1c3@gmail.com>
2024-11-04 12:17:27 +0000

    routes

---
+++ scouting_app/lib/jogador.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class JogadorPage extends StatefulWidget {
+  @override
+  _JogadorPageState createState() => _JogadorPageState();
+}
+
+class _JogadorPageState extends State<JogadorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jogador'),
+      ),
+      body: Center(
+        child: Text('This is the Jogador page'),
+      ),
+    );
+  }
+}
