f6d88be11e2174a4eb88e6e0bfef5399af0bce26
Franciscaa77 <kikapalma004@gmail.com>
2024-11-04 15:42:09 +0000

    Update perfil.dart

--- scouting_app/lib/perfil.dart
+++ scouting_app/lib/perfil.dart
@@ -19,7 +19,7 @@ class _PerfilPageState extends State<PerfilPage> {
         children: [
           ListTile(
             title: Text(
-              'Mudar palavra passe',
+              'Contactos',
               style: TextStyle(color: Colors.white),
             ),
             trailing: Icon(Icons.chevron_right, color: Colors.grey),
