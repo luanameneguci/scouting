9ae66195e46dfbd38dc836533f2d49216fcba84c
pt1C3 <rafaelsilva1c3@gmail.com>
2024-11-11 15:11:26 +0000

    sim

--- scouting_app/lib/politicasPriv.dart
+++ scouting_app/lib/politicasPriv.dart
@@ -7,15 +7,40 @@ class PoliticasPrivPage extends StatefulWidget {
   _PoliticasPrivPageState createState() => _PoliticasPrivPageState();
 }
 
-class _PoliticasPrivPageState extends State<PoliticasPrivPage>{
+class _PoliticasPrivPageState extends State<PoliticasPrivPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Política de Privacidade'),
       ),
-      body: const Center(
-        child: Text('This is the política de privacidade page. Quem vai realizar esta página é Rafael Silva 25422'),
+      body: ListView(
+        children: <Widget>[
+          ExpansionTile(
+            title: const Text('Section 1'),
+            children: <Widget>[
+              ListTile(
+                title: const Text('Content for Section 1'),
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('Section 2'),
+            children: <Widget>[
+              ListTile(
+                title: const Text('Content for Section 2'),
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('Section 3'),
+            children: <Widget>[
+              ListTile(
+                title: const Text('Content for Section 3'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
