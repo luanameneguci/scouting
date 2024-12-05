92d02290532d72ba67d51985ef30b80b7e10f161
raso04 <pv26511@alunos.estgv.ipv.pt>
2024-12-05 15:18:35 +0000

    sem bootstrap

--- frontend/src/views/dashboard.js
+++ frontend/src/views/dashboard.js
@@ -1,8 +1,6 @@
 import './dashboard.css';
 
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 export default function Dashboard(){
     <div>
     <title>Dashboard</title>
