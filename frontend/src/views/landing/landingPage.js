d4ece2dde2252904834ceff2c33212416829a0d6
raso04 <pv26511@alunos.estgv.ipv.pt>
2024-12-16 14:57:44 +0000

    landing

--- frontend/src/views/landing/landingPage.js
+++ frontend/src/views/landing/landingPage.js
@@ -1,23 +1,36 @@
 import React from 'react';
+import Slider from "react-slick";
 import HeroImage from '../../assets/heroImage.png';
 import './landingPage.css';
+import Image1 from '../../assets/image1.png';
+import Image2 from '../../assets/image2.png';
+import Image3 from '../../assets/image3.png';
+import Image4 from '../../assets/image4.png';
+import Image5 from '../../assets/image5.png';
 
 export default function LandingPage() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    };
+
     return (
         <>
             <div className="landing-page">
-
                 <div className='landing-content hero-section'>
                     <img src={HeroImage} alt="Hero Image" />
                     <div className="hero-content">
                         <h1>DESCOBRE ESTRELAS, AVALIA TALENTOS E CONSTRÓI O AMANHÃ</h1>
                         <p>
-                        Cria relatórios pormenorizados, monitoriza o desempenho e avalia cada partida com precisão. Regista a evolução dos jogadores e garante que o talento do Académico de Viseu seja potenciado ao máximo, jogo após jogo, temporada após temporada.
+                            Cria relatórios pormenorizados, monitoriza o desempenho e avalia cada partida com precisão. Regista a evolução dos jogadores e garante que o talento do Académico de Viseu seja potenciado ao máximo, jogo após jogo, temporada após temporada.
                         </p>
                         <button className="landing-button">DESCARREGA AGORA</button>
                     </div>
-
-
                 </div>
                 <div className="landing-features">
                     <div className="feature-item">
@@ -36,8 +49,69 @@ export default function LandingPage() {
                         <p>Ajudamos a moldar o futuro dos próximos jogadores e do clube.</p>
                     </div>
                 </div>
+
+                <div className="carousel-section">
+                    <Slider {...settings}>
+                        <div className="carousel-item">
+                            <img src={Image1} alt="Slide 1" />
+                            <div className="carousel-caption">
+                                <h3>PÁGINA DO LOGIN</h3>
+                                <p>Na app Viriatos Scouting, tens tudo o que precisas para estar sempre a par das novidades do clube e dos teus atletas favoritos. Revê momentos importantes e acede a conteúdos exclusivos, onde e quando quiseres.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Image2} alt="Slide 2" />
+                            <div className="carousel-caption">
+                                <h3>Slide 2</h3>
+                                <p>Descrição do Slide 2</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Image3} alt="Slide 3" />
+                            <div className="carousel-caption">
+                                <h3>Slide 3</h3>
+                                <p>Descrição do Slide 3</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Image4} alt="Slide 4" />
+                            <div className="carousel-caption">
+                                <h3>Slide 4</h3>
+                                <p>Descrição do Slide 4</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Image5} alt="Slide 5" />
+                            <div className="carousel-caption">
+                                <h3>Slide 5</h3>
+                                <p>Descrição do Slide 5</p>
+                            </div>
+                        </div>
+                    </Slider>
+                </div>
             </div>
         </>
     );
-};
+}
 
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "black" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "black" }}
+            onClick={onClick}
+        />
+    );
+}
