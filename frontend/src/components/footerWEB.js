0a90da1e702e6c1ada72e2d764ba21c4f6256977
Franciscaa77 <kikapalma004@gmail.com>
2025-01-16 17:07:29 +0000

    sory

--- frontend/src/components/footerWEB.js
+++ frontend/src/components/footerWEB.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './footerWEB.css';
 import { NavLink } from "react-router-dom";
 import logoSVG from '../assets/whiteLogo.svg';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function Footer() {
   return (
@@ -9,23 +11,23 @@ function Footer() {
       {/* Links principais */}
       <div className="footer-content">
         <a href="#" className="footer-link">PÁGINA PRINCIPAL</a>
-        <a href="#" className="footer-link">POLÍTICAS DE PRIVACIDADE</a>
-        <a href="#" className="footer-link">CONTACTOS</a>
+        <a href="/Privacidade" className="footer-link">POLÍTICAS DE PRIVACIDADE</a>
+        <a href="/Contactos" className="footer-link">CONTACTOS</a>
       </div>
 
       {/* Logo ao centro */}
       <div className="footer-logo">
-        <img src="whiteLogo.png" alt="Logo Académico de Viseu FC" className="logo" />
+        <img src={logoSVG} alt="Logo Académico de Viseu FC" />
       </div>
 
       {/* Texto de copyright e ícones sociais */}
       <div className="footer-bottom">
         <span>COPYRIGHT 2024 © ACADÉMICO DE VISEU FC</span>
         <div className="social-icons">
-          <a href="https://instagram.com" target="_blank" rel="noreferrer">
+          <a href="https://www.instagram.com/academicodeviseufc/" target="_blank" rel="noreferrer">
             <i className="fab fa-instagram"></i>
           </a>
-          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+          <a href="https://www.facebook.com/AcademicodeViseu" target="_blank" rel="noreferrer">
             <i className="fab fa-facebook"></i>
           </a>
         </div>
@@ -33,5 +35,4 @@ function Footer() {
     </footer>
   );
 }
-
 export default Footer;
