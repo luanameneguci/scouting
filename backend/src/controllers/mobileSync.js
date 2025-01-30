db46991ceded4b6f7bfc3c4f6f24e1b513f1e635
luanameneguci <133794772+luanameneguci@users.noreply.github.com>
2025-01-30 21:16:29 +0000

    oij

--- backend/src/controllers/mobileSync.js
+++ backend/src/controllers/mobileSync.js
@@ -8,6 +8,14 @@ const controllers = {};
 controllers.mobileSync = async (req, res) => {
     try{
         const EquipaAtleta = await models.EquipaAtleta.findAll();
+        const EscalaoDivisao = await models.EscalaoDivisao.findAll();
+        const JogoAtleta = await models.JogoAtleta.findAll();
+        const JogoClube = await models.JogoClube.findAll();
+        const UtilizadorJogo = await models.UtilizadorJogo.findAll();
+        const atleta = await models.atleta.findAll();
+        const clube = await models.clube.findAll();
+        const divisao = await models.divisao.findAll();
+  
     }
     catch{}
 }
