72f316cd84a5db0faa76ecd96057ef6c2ec4498b
SlicedBlueApple <likebossi666@gmail.com>
2025-02-04 00:11:01 +0000

    Update database.js

--- backend/src/models/database.js
+++ backend/src/models/database.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
     'scouting',
     'postgres',
-    'postgres', // postgres
+    '123', // postgres | Se estiver isto aqui, é porque me esqueci de meter "postgres" outra vez, desculpem T-T  ~Sliced
     {
         host: 'localhost',
         port: '5432',
